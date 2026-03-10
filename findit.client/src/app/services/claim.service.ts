9845492cdcc4478d56036548e7f1e3bdd03bc1fa
kajal-1402-yadav <kajall1402@gmail.com>
2026-03-10 11:27:24 +0530

    Initial backend implementation for FindIt (Auth, Items, Claims APIs)

---
+++ findit.client/src/app/services/claim.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClaimService {
+
+  private apiUrl = '/api/claims';
+
+  constructor(private http: HttpClient) { }
+
+  createClaim(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  getClaimsForItem(itemId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/item/${itemId}`);
+  }
+
+  getClaimsByUser(userId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/user/${userId}`);
+  }
+
+  approveClaim(id: number): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}/approve`, {});
+  }
+
+  rejectClaim(id: number): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}/reject`, {});
+  }
+}
